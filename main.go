ca8e6bda28602fd9d38164fae9a0188bf56f56c4
Loh Siu Yin <siuyin@beyondbroadcast.com>
2017-11-07 05:36:44 +0000

    Refactored code.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -11,11 +12,30 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
+// N is the names struct.
 type N struct {
 	S []string `yaml:"surnames"`
 	G []string `yaml:"givennames"`
 }
 
+func getNames(r io.Reader) (N, error) {
+	var nam N
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nam, err
+	}
+
+	if err := yaml.Unmarshal(b, &nam); err != nil {
+		return nam, err
+	}
+	return nam, nil
+}
+func randSurName(n N) string {
+	return n.S[rand.Intn(len(n.S))]
+}
+func randGivenName(n N) string {
+	return n.G[rand.Intn(len(n.G))]
+}
 func main() {
 	fmt.Println("random name generator")
 	file, err := os.Open("data.yaml")
@@ -24,20 +44,13 @@ func main() {
 	}
 	defer file.Close()
 
-	b, err := ioutil.ReadAll(file)
+	nam, err := getNames(file)
 	if err != nil {
-		log.Fatal("ERROR: failed to read from file: ", err)
-	}
-
-	var nam N
-	if err := yaml.Unmarshal(b, &nam); err != nil {
-		log.Fatal("ERROR: could not unmarshal yaml: ", err)
+		log.Fatal(err)
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	lSur := len(nam.S)
-	lGiv := len(nam.G)
 	for i := 0; i < 20; i++ {
-		fmt.Println(nam.S[rand.Intn(lSur)], nam.G[rand.Intn(lGiv)], nam.G[rand.Intn(lGiv)])
+		fmt.Println(randSurName(nam), randGivenName(nam), randGivenName(nam))
 	}
 }
